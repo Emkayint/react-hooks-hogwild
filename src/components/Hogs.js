e0e1a82e1be2e43c091ae7ee992b578ba00f7f9b
Martin <mkmartinoes@gmail.com>
2022-05-07 11:57:24 +0300

    Hero section

--- src/components/Hogs.js
+++ src/components/Hogs.js
@@ -1,10 +1,16 @@
 
-function Hogs({hogs}){
+function Hogs({hogs, name, image, weight, medal, greased}){
 
 
   return (
-    <div >
-
+    <div className="Hero">
+      <h4>{name}</h4>
+      <img src = {image} alt = "Alt mage" />
+      <div className =''>
+        <li>{ weight }</li>
+        <li>{ greased ? "Greased" : "Not Greased" }</li>
+        <li> { medal }</li>
+      </div>
     </div>
   )
 
